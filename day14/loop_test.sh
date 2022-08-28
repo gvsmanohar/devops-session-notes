b3dd1f13cce08eebd5bc86614b92e05ebbe95bfd
santhosh kumar <santhu3064@gmail.com>
2022-08-28 10:14:55 +0100

    Day 14 uploaded

---
+++ day14/loop_test.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# while :
+# do
+#   echo "Infinite loop"
+# done
+
+
+until false
+do
+  echo "Infinite loop"
+done
---
+++ day14/notes.md
@@ -0,0 +1,16 @@
+
+
+# Loops
+
+# while and until
+
+```
+while [[ condtion ]]
+do
+ statements
+done
+
+
+
+
+# functions
